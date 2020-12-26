902b8bef1595ca1373056dcb03735f022754fd61
Isaac Bergl <& d:/OneDrive/git/Poker/.venv/Scripts/python.exe d:/OneDrive/git/Poker/main.pyIsaac Bergl>
2020-12-26 18:17:44 +0800

    Pause Screen + bugs

--- GUI.py
+++ GUI.py
@@ -62,33 +62,19 @@ def backtrackVisualise(board):
         board[posr][posc] = num
 
 
-        for r in range(9):
-            for c in range(9):
-                
-                if board[r][c] != 0:
-                    displayNum(board[r][c], r, c)   
-
-        for r in range(9):  
-            thickness = 1  
-            if r%3 == 0:
-                thickness = 2  
-            pygame.draw.line(screen,BLACK,(r*cell_size, 0), (r*cell_size, 9*cell_size),thickness)
-        for c in range(10): 
-            thickness = 1  
-            if c%3 == 0:
-                thickness = 2       
-            pygame.draw.line(screen,BLACK,(0, c*cell_size), (9*cell_size, c*cell_size),thickness)
+        drawBoard(screen)
     
         pygame.display.update()
 #ghost entries
 ghost = []
     
-
-for r in range(9):
-    row = []
-    for c in range(9):
-        row.append([0,0,0,0,0,0,0,0,0])
-    ghost.append(row)
+def init_ghost(ghost):
+    for r in range(9):
+        row = []
+        for c in range(9):
+            row.append([0,0,0,0,0,0,0,0,0])
+        ghost.append(row)
+init_ghost(ghost)
 
 def ghostEntry():
 
@@ -103,29 +89,117 @@ def ghostEntry():
                     num_font = pygame.font.Font('freesansbold.ttf', 20)
                     ghost_num_dsp = num_font.render(str(v), True, (100,100,100))
                     screen.blit(ghost_num_dsp, (c_big+3, r_big+3))
-                
+#PAUSE SCREEN
+
+def pauseScreen(screen):
+    PAUSED = True
+    while PAUSED:
+
+        drawBoard(screen)
+
+       
+        pygame.draw.rect(screen,BLACK,(150, 150, 240, 235))
+        #TOP - RETURN
+        RETURN_BOX = pygame.Rect((152, 152, 236, 57))
+        pygame.draw.rect(screen,WHITE, RETURN_BOX)       
+        num_font = pygame.font.Font('freesansbold.ttf', 24)
+        static_num_dsp = num_font.render("Return (esc)", True, (0,0,0))
+        screen.blit(static_num_dsp, (210, 170))
+        #MIDDLE - RESET
+        RESET_BOX = pygame.Rect((152, 211, 236, 56)) 
+        pygame.draw.rect(screen,WHITE, RESET_BOX)
+        num_font = pygame.font.Font('freesansbold.ttf', 24)
+        static_num_dsp = num_font.render("Reset Game", True, (0,0,0))
+        screen.blit(static_num_dsp, (210, 227))
+        #MIDDLE - NEW
+        NEW_BOX = pygame.Rect((152, 269, 236, 56)) 
+        pygame.draw.rect(screen,WHITE,NEW_BOX)
+        num_font = pygame.font.Font('freesansbold.ttf', 24)
+        static_num_dsp = num_font.render("New Game", True, (0,0,0))
+        screen.blit(static_num_dsp, (210, 283))
+
+        #QUIT
+        QUIT_BOX = pygame.Rect((152, 327, 236, 56)) 
+        pygame.draw.rect(screen,WHITE,QUIT_BOX)
+        num_font = pygame.font.Font('freesansbold.ttf', 24)
+        static_num_dsp = num_font.render("QUIT", True, (0,0,0))
+        screen.blit(static_num_dsp, (210, 339))
+
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                PAUSED, running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                x = mouse_pos[0]
+                y = mouse_pos[1]
+                mouse = pygame.Rect(x, y, 0,0) 
+                if RETURN_BOX.collidepoint(x,y):
+                    PAUSED = False
+                if NEW_BOX.collidepoint(x,y):
+                    global board
+                    global ghost
+                    board = sodoku.init_arr()
+                    ghost = []
+                    init_ghost(ghost)
+                    PAUSED = False
+                if QUIT_BOX.collidepoint(x,y):
+                    PAUSED, running = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                PAUSED = False
+
+
+        pygame.display.update()
+
+
+
+    
+
+
+def drawBoard(screen):
+    for r in range(9):
+        for c in range(9):
+            if board[r][c] != 0:
+                displayNum(board[r][c], r, c)   
+
+    ghostEntry()
+
+    for r in range(9):  
+        thickness = 1  
+        if r%3 == 0:
+            thickness = 2  
+        pygame.draw.line(screen,BLACK,(r*cell_size, 0), (r*cell_size, 9*cell_size),thickness)
+    for c in range(10): 
+        thickness = 1  
+        if c%3 == 0:
+            thickness = 2       
+        pygame.draw.line(screen,BLACK,(0, c*cell_size), (9*cell_size, c*cell_size),thickness)
                        
 #Game Loop
 highlightcell = (-1,-1)
 oldcell = (-1,-1)
 numbers = (pygame.K_1, pygame.K_2, pygame.K_3, pygame.K_4, pygame.K_5,pygame.K_6, pygame.K_7, pygame.K_8, pygame.K_9 )
 while running:
-    screen.fill(WHITE)
-
-     
+    screen.fill(WHITE)    
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_pos = pygame.mouse.get_pos()
-            print("Mouse")
+            
             oldcell = highlightcell
             highlightcell = (int(mouse_pos[0]/cell_size), int(mouse_pos[1]/cell_size))
+            if highlightcell[0] > 8 or highlightcell[1] > 8:
+                highlightcell = (-1,-1)
+            print("Mouse (" + str(highlightcell[0]) + ", " + str(highlightcell[1]) + ")")
         if event.type == pygame.KEYDOWN:
             c = int(highlightcell[0])
             r = int(highlightcell[1])
            # print(highlightcell)
+
+            if event.key == pygame.K_ESCAPE:
+                pauseScreen(screen)
             
             if event.key in numbers and pygame.key.get_mods() & pygame.KMOD_LCTRL:
                 
@@ -154,29 +228,8 @@ while running:
         highlightcell=(1000,1000)
         highlightCell(oldcell, WHITE)
         highlightCell(highlightcell, YELLOW)
-        
 
-
-    for r in range(9):
-        for c in range(9):
-            if board[r][c] != 0:
-                displayNum(board[r][c], r, c)   
-
-    ghostEntry()
-
-    for r in range(9):  
-        thickness = 1  
-        if r%3 == 0:
-            thickness = 2  
-        pygame.draw.line(screen,BLACK,(r*cell_size, 0), (r*cell_size, 9*cell_size),thickness)
-    for c in range(10): 
-        thickness = 1  
-        if c%3 == 0:
-            thickness = 2       
-        pygame.draw.line(screen,BLACK,(0, c*cell_size), (9*cell_size, c*cell_size),thickness)
-
-    
-        
+    drawBoard(screen)          
     pygame.display.update()
 
     GAMEOVERSCREEN = True
