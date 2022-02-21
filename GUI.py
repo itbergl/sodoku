02b0c483f6c7877448747c332d68b501da5d51f3
itbergl <tberg644@gmail.com>
2022-02-21 16:44:24 +0800

    improved GUI design

--- GUI.py
+++ GUI.py
@@ -12,19 +12,20 @@ pygame.init()
 
 #Screen
 cell_size = 60
-screen = pygame.display.set_mode((9*cell_size,10*cell_size))
+screen = pygame.display.set_mode((9 * cell_size, 10 * cell_size))
 
 #Icon
 pygame.display.set_caption("Sodoku Player")
-icon = pygame.image.load("dep/sudoku.png")
+icon = pygame.image.load("sudoku.png")
 pygame.display.set_icon(icon)
 
 #Colours
-BLACK = pygame.Color((0,0,0))
-WHITE = pygame.Color((255,255,255))
-GREY = pygame.Color((200,200,200))
+BLACK = pygame.Color((0, 0, 0))
+WHITE = pygame.Color((255, 255, 255))
+GREY = pygame.Color((200, 200, 200))
 YELLOW = pygame.Color((255, 255, 153))
-DULLRED = pygame.Color((200,200,255))
+DULLRED = pygame.Color((200, 200, 255))
+BLUE = pygame.Color((204, 255, 255))
 
 #initialise board
 board = sodoku.init_arr()
@@ -49,28 +50,31 @@ GAMEOVERSCREEN = True
 TOGGLECTRL = False
 TIMERSTARTED = False
 
+
 #METHODS
 def displayNum(num, r, c, colour):
     num_font = pygame.font.Font('freesansbold.ttf', 40)
     static_num_dsp = num_font.render(str(num), True, colour)
-    screen.blit(static_num_dsp, ((c+0.25)*cell_size, (r+0.25)*cell_size))
+    screen.blit(static_num_dsp,
+                ((c + 0.25) * cell_size, (r + 0.25) * cell_size))
+
 
 def highlightCell(coord, COL):
-    xpos = coord[0]*cell_size
-    ypos = coord[1]*cell_size
-    pygame.draw.rect(screen,COL,(xpos, ypos, cell_size, cell_size))
+    xpos = coord[0] * cell_size
+    ypos = coord[1] * cell_size
+    pygame.draw.rect(screen, COL, (xpos, ypos, cell_size, cell_size))
 
 
 def backtrackVisualise():
-    global screen   
+    global screen
     global board
     global SIMULATED
-    
+
     SIMULATED = True
     SIMULATE = True
 
     answer_list = []
-    
+
     solved_board = sodoku.runRecursiveHistory(board, answer_list)
 
     index = 0
@@ -85,12 +89,12 @@ def backtrackVisualise():
                 board = solved_board
                 NEXTSTEP = False
                 SIMULATE = False
-                             
+
         if NEXTSTEP:
-            if len(answer_list) >= index-1:
+            if len(answer_list) >= index - 1:
                 string = answer_list[index]
                 index += 1
-            
+
                 num = int(string[0])
                 posr = int(string[1])
                 posc = int(string[2])
@@ -99,19 +103,21 @@ def backtrackVisualise():
             else:
                 SIMULATE = False
 
-        drawBoard()  
+        drawBoard()
         pygame.display.update()
 
-#Ghost entries    
+
+#Ghost entries
 def init_ghost():
     global ghost
     ghost = []
     for r in range(9):
         row = []
         for c in range(9):
-            row.append([0,0,0,0,0,0,0,0,0])
+            row.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
         ghost.append(row)
-   
+
+
 def init_badEntry():
     global badEntry
     for r in range(9):
@@ -120,22 +126,24 @@ def init_badEntry():
             row.append(0)
         badEntry.append(row)
 
-def ghostEntry():
 
+def ghostEntry():
     for r in range(9):
         for c in range(9):
             for v in ghost[r][c]:
                 if v != 0:
 
-                    c_big = c*cell_size + ((v-1)%3)*cell_size/3
-                    r_big = r*cell_size + (int((v-1)/3))*cell_size/3
+                    c_big = c * cell_size + ((v - 1) % 3) * cell_size / 3
+                    r_big = r * cell_size + (int((v - 1) / 3)) * cell_size / 3
 
                     num_font = pygame.font.Font('freesansbold.ttf', 20)
-                    ghost_num_dsp = num_font.render(str(v), True, (100,100,100))
-                    screen.blit(ghost_num_dsp, (c_big+3, r_big+3))
+                    ghost_num_dsp = num_font.render(str(v), True,
+                                                    (100, 100, 100))
+                    screen.blit(ghost_num_dsp, (c_big + 3, r_big + 3))
+
 
 #PAUSE SCREEN
-def pauseScreen():   
+def pauseScreen():
     global GAMEOVERSCREEN
     global screen
     global RUNNING
@@ -145,37 +153,37 @@ def pauseScreen():
 
     PAUSED = True
     while PAUSED:
-        drawBoard()       
-        pygame.draw.rect(screen,BLACK,(150, 150, 240, 235))
+        drawBoard()
+        pygame.draw.rect(screen, BLACK, (150, 150, 240, 235))
 
         #TOP - RETURN
         RETURN_BOX = pygame.Rect((152, 152, 236, 57))
-        pygame.draw.rect(screen,WHITE, RETURN_BOX)       
+        pygame.draw.rect(screen, WHITE, RETURN_BOX)
         num_font = pygame.font.Font('freesansbold.ttf', 24)
-        static_num_dsp = num_font.render("Return (esc)", True, (0,0,0))
+        static_num_dsp = num_font.render("Return (esc)", True, (0, 0, 0))
         screen.blit(static_num_dsp, (210, 170))
 
         #MIDDLE - RESET
-        RESET_BOX = pygame.Rect((152, 211, 236, 56)) 
-        pygame.draw.rect(screen,WHITE, RESET_BOX)
+        RESET_BOX = pygame.Rect((152, 211, 236, 56))
+        pygame.draw.rect(screen, WHITE, RESET_BOX)
         num_font = pygame.font.Font('freesansbold.ttf', 24)
-        static_num_dsp = num_font.render("Reset Game", True, (0,0,0))
+        static_num_dsp = num_font.render("Reset Game", True, (0, 0, 0))
         screen.blit(static_num_dsp, (210, 227))
 
         #MIDDLE - NEW
-        NEW_BOX = pygame.Rect((152, 269, 236, 56)) 
-        pygame.draw.rect(screen,WHITE,NEW_BOX)
+        NEW_BOX = pygame.Rect((152, 269, 236, 56))
+        pygame.draw.rect(screen, WHITE, NEW_BOX)
         num_font = pygame.font.Font('freesansbold.ttf', 24)
-        static_num_dsp = num_font.render("New Game", True, (0,0,0))
+        static_num_dsp = num_font.render("New Game", True, (0, 0, 0))
         screen.blit(static_num_dsp, (210, 283))
 
         #QUIT
-        QUIT_BOX = pygame.Rect((152, 327, 236, 56)) 
-        pygame.draw.rect(screen,WHITE,QUIT_BOX)
+        QUIT_BOX = pygame.Rect((152, 327, 236, 56))
+        pygame.draw.rect(screen, WHITE, QUIT_BOX)
         num_font = pygame.font.Font('freesansbold.ttf', 24)
-        static_num_dsp = num_font.render("QUIT", True, (0,0,0))
+        static_num_dsp = num_font.render("QUIT", True, (0, 0, 0))
         screen.blit(static_num_dsp, (210, 339))
-       
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 PAUSED, RUNNING = False
@@ -183,23 +191,23 @@ def pauseScreen():
                 mouse_pos = pygame.mouse.get_pos()
                 x = mouse_pos[0]
                 y = mouse_pos[1]
-                mouse = pygame.Rect(x, y, 0,0) 
-                if RETURN_BOX.collidepoint(x,y):
+                mouse = pygame.Rect(x, y, 0, 0)
+                if RETURN_BOX.collidepoint(x, y):
                     PAUSED = False
-                if NEW_BOX.collidepoint(x,y):
-                    
+                if NEW_BOX.collidepoint(x, y):
+
                     board = sodoku.init_arr()
                     board_copy = np.copy(board)
                     ghost = []
                     GAMEOVERSCREEN = False
                     init_ghost()
                     PAUSED = False
-                if QUIT_BOX.collidepoint(x,y):
+                if QUIT_BOX.collidepoint(x, y):
                     PAUSED = False
                     RUNNING = False
-                    
+
                     GAMEOVERSCREEN = False
-                if RESET_BOX.collidepoint(x,y):
+                if RESET_BOX.collidepoint(x, y):
                     PAUSED = False
                     board = np.copy(board_copy)
                     ghost = []
@@ -209,12 +217,12 @@ def pauseScreen():
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 PAUSED = False
 
-
         pygame.display.update()
+
 
 def drawBoard():
     global screen
-    
+    ghostEntry()
     for r in range(9):
         for c in range(9):
             if badEntry[r][c] != 0:
@@ -225,164 +233,187 @@ def drawBoard():
 
     for r in range(9):
         for c in range(9):
-            if board[r][c] != 0:     
-                displayNum(board[r][c], r, c, (0,0,0))
-            if badEntry[r][c] != 0:  
-                    displayNum(badEntry[r][c], r, c, (255,0,0)) 
-    
+            if board_copy[r][c] != 0:
+                highlightCell((c, r), BLUE)
 
-    ghostEntry()
+            if board[r][c] != 0:
+                displayNum(board[r][c], r, c, (0, 0, 0))
 
-    for r in range(9):  
-        thickness = 1  
-        if r%3 == 0:
-            thickness = 2  
-        pygame.draw.line(screen,BLACK,(r*cell_size, 0), (r*cell_size, 9*cell_size),thickness)
-    for c in range(10): 
-        thickness = 1  
-        if c%3 == 0:
-            thickness = 2       
-        pygame.draw.line(screen,BLACK,(0, c*cell_size), (9*cell_size, c*cell_size),thickness)
+            if badEntry[r][c] != 0:
+                displayNum(badEntry[r][c], r, c, (255, 0, 0))
+
+    for r in range(9):
+        thickness = 1
+        if r % 3 == 0:
+            thickness = 2
+        pygame.draw.line(screen, BLACK, (r * cell_size, 0),
+                         (r * cell_size, 9 * cell_size), thickness)
+    for c in range(10):
+        thickness = 1
+        if c % 3 == 0:
+            thickness = 2
+        pygame.draw.line(screen, BLACK, (0, c * cell_size),
+                         (9 * cell_size, c * cell_size), thickness)
+
 
 def drawBottomText():
     global screen
 
     num_font = pygame.font.Font('freesansbold.ttf', 16)
-    #Space     
-    static_num_dsp = num_font.render("Space: start solve (press again to skip)", True, (0,0,0))
+    #Space
+    static_num_dsp = num_font.render(
+        "Space: start solve (press again to skip)", True, (0, 0, 0))
     screen.blit(static_num_dsp, (0, 550))
 
-    #GHOST    
-    static_num_dsp = num_font.render("Shift + num: ghost entry", True, (0,0,0))
+    #GHOST
+    static_num_dsp = num_font.render("Shift + num: ghost entry", True,
+                                     (0, 0, 0))
     screen.blit(static_num_dsp, (350, 550))
 
     #PAUSE
-    static_num_dsp = num_font.render("Esc: open menu", True, (0,0,0))
+    static_num_dsp = num_font.render("Esc: open menu", True, (0, 0, 0))
     screen.blit(static_num_dsp, (0, 580))
 
     #BACKSPACE
-    static_num_dsp = num_font.render("Backspace: erase entry", True, (0,0,0))
+    static_num_dsp = num_font.render("Backspace: erase entry", True, (0, 0, 0))
     screen.blit(static_num_dsp, (350, 580))
+
 
 def printClock(time):
     global screen
 
     #print box
-    TIME_BOX = pygame.Rect((210, 570, 120, 30)) 
-    pygame.draw.rect(screen,BLACK,TIME_BOX)
+    TIME_BOX = pygame.Rect((210, 570, 120, 30))
+    pygame.draw.rect(screen, BLACK, TIME_BOX)
 
     #print time
     num_font = pygame.font.Font('freesansbold.ttf', 20)
-    static_num_dsp = num_font.render(str(time/1000), True, WHITE)
+    static_num_dsp = num_font.render(str(time / 1000), True, WHITE)
     screen.blit(static_num_dsp, (250, 574))
+
 
 def gameOverLoop():
     global screen
     global GAMEOVERSCREEN
     global RUNNING
     if sodoku.nextBlank(board) is None:
-   
+
         while GAMEOVERSCREEN:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    GAMEOVERSCREEN = False                  
+                    GAMEOVERSCREEN = False
                     RUNNING = False
                 if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                     pauseScreen()
-            pygame.draw.rect(screen,BLACK,(0, 540, 540, 60))
-            pygame.draw.rect(screen,WHITE,(2, 542, 536, 66))
+            pygame.draw.rect(screen, BLACK, (0, 540, 540, 60))
+            pygame.draw.rect(screen, WHITE, (2, 542, 536, 66))
             num_font = pygame.font.Font('freesansbold.ttf', 40)
-           
+
             if SIMULATED:
-                static_num_dsp = num_font.render("COMPUTER WINS!", True, (0,0,0))
+                static_num_dsp = num_font.render("COMPUTER WINS!", True,
+                                                 (0, 0, 0))
                 screen.blit(static_num_dsp, (105, 550))
             else:
-                static_num_dsp = num_font.render("YOU WIN!    " + str(passed_time/1000) + "s", True, (0,0,0))
+                static_num_dsp = num_font.render(
+                    "YOU WIN!    " + str(passed_time / 1000) + "s", True,
+                    (0, 0, 0))
                 screen.blit(static_num_dsp, (80, 550))
-            
-            
-            pygame.display.update() 
+
+            pygame.display.update()
 
 
 #Game Loop
-highlightcell = (-1,-1)
-oldcell = (-1,-1)
-numbers = (pygame.K_1, pygame.K_2, pygame.K_3, pygame.K_4, pygame.K_5,pygame.K_6, pygame.K_7, pygame.K_8, pygame.K_9 )
-
+highlightcell = (-1, -1)
+oldcell = (-1, -1)
+numbers = [
+    pygame.K_1, pygame.K_2, pygame.K_3, pygame.K_4, pygame.K_5, pygame.K_6,
+    pygame.K_7, pygame.K_8, pygame.K_9
+]
 
 init_ghost()
 init_badEntry()
 
 while RUNNING:
     passed_time += clock.get_time()
-    screen.fill(WHITE)    
+    screen.fill(WHITE)
 
     for event in pygame.event.get():
         #close game
         if event.type == pygame.QUIT:
             RUNNING = False
         #click for L and R mouse click
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button in (1,3):
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button in (1, 3):
             mouse_pos = pygame.mouse.get_pos()
-                  
+
             oldcell = highlightcell
-            highlightcell = (int(mouse_pos[0]/cell_size), int(mouse_pos[1]/cell_size))
+            highlightcell = (int(mouse_pos[0] / cell_size),
+                             int(mouse_pos[1] / cell_size))
             if highlightcell[0] > 8 or highlightcell[1] > 8:
-                highlightcell = (-1,-1)
-            
+                highlightcell = (-1, -1)
+
             #print(pygame.mouse.get_pressed())
         if event.type == pygame.KEYDOWN:
             c = int(highlightcell[0])
             r = int(highlightcell[1])
-           
+
             # pause game
             if event.key == pygame.K_ESCAPE:
                 pauseScreen()
-            
+
             #input ghost entry
-            if (event.key in numbers or event.key == pygame.K_BACKSPACE) and pygame.key.get_mods() & pygame.KMOD_LCTRL:
-  
+            if (event.key in numbers or event.key == pygame.K_BACKSPACE
+                ) and pygame.key.get_mods() & pygame.KMOD_LCTRL:
+
+                if event.key == pygame.K_BACKSPACE:
+                    ghost[r][c] = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+                    continue
+
                 value = int(pygame.key.name(event.key))
 
                 #print("CTRL " + str(value))
                 if value not in ghost[r][c]:
-                    ghost[r][c][value-1]=value
+                    ghost[r][c][value - 1] = value
                 else:
-                    ghost[r][c][value-1] = 0
+                    ghost[r][c][value - 1] = 0
 
             #input answer numbers
             elif event.key in numbers or event.key == pygame.K_BACKSPACE:
 
-                value = int(pygame.key.name(event.key))
+                if event.key == pygame.K_BACKSPACE:
+                    value = 0
+                else:
+                    value = int(pygame.key.name(event.key))
 
                 if board_copy[r][c] == 0:
-                    if sodoku.addEntry(board, r, c, value):                        
-                        ghost[r][c] = [0,0,0,0,0,0,0,0,0]
+
+                    if sodoku.addEntry(board, r, c, value):
+                        # ghost[r][c] = [0, 0, 0, 0, 0, 0, 0, 0, 0]
                         badEntry[r][c] = 0
-                    elif board[r][c]==0 and board_copy[r][c]==0:
-                        badEntry[r][c] = value                   
+                    elif board[r][c] == 0 and board_copy[r][c] == 0:
+                        badEntry[r][c] = value
 
                 #print(str(value))
 
             #run backtracking algorithm visualisation animation
-            elif event.key == pygame.K_SPACE: 
-                init_ghost()               
-                backtrackVisualise() 
+            elif event.key == pygame.K_SPACE:
+                init_ghost()
+                backtrackVisualise()
                 #print("Space")
+
             #Backspace if it is a writable cell
             elif event.key == pygame.K_BACKSPACE or event.key == pygame.K_0:
                 if board_copy[r][c] == 0:
                     board[r][c] = 0
-                 
+
     if oldcell != highlightcell:
         highlightCell(oldcell, WHITE)
         highlightCell(highlightcell, YELLOW)
     else:
-        highlightcell=(1000,1000)
+        highlightcell = (1000, 1000)
         highlightCell(oldcell, WHITE)
         highlightCell(highlightcell, YELLOW)
 
-    drawBoard()  
+    drawBoard()
     drawBottomText()
     printClock(passed_time)
     clock.tick(30)
@@ -390,6 +421,3 @@ while RUNNING:
     gameOverLoop()
 
     pygame.display.update()
-
-    
-   
--- sodoku.py
+++ sodoku.py
@@ -4,6 +4,7 @@ import multiprocessing
 import numpy as np
 import random
 
+
 # initialise blank sodoku array
 def init_arr():
     sodoku = []
@@ -15,24 +16,29 @@ def init_arr():
             row.append(0)
         sodoku.append(row)
 
-    Sample_file = open("dep/Samples.txt","r")
+    Sample_file = open("samples.txt", "r")
     list = Sample_file.readlines()
-    
+
     Sample_file.close()
     return fill_in(sodoku, random.choice(list))
+
 
 # turn string representation into a board
 def fill_in(sodoku, string_rep):
     counter = 0
     for row in range(9):
         for col in range(9):
-            if (string_rep[counter]!='.'):
+            if (string_rep[counter] != '.'):
                 sodoku[row][col] = int(string_rep[counter])
             counter += 1
     return sodoku
 
+
 # check if entry is valid
 def isValid(sodoku, row, col, value):
+
+    if value == 0:
+        return True
 
     if sodoku[row][col] != 0:
         return False
@@ -41,23 +47,23 @@ def isValid(sodoku, row, col, value):
         return False
 
     for i in range(9):
-        if sodoku[row][i]==value:
+        if sodoku[row][i] == value:
             return False
-    
+
     for i in range(9):
-        if sodoku[i][col]==value:
+        if sodoku[i][col] == value:
             return False
-    
-    row_offset = row - row%3
-    col_offset = col - col%3
+
+    row_offset = row - row % 3
+    col_offset = col - col % 3
 
     for i in range(3):
         for j in range(3):
-            if sodoku[row_offset+i][col_offset+j]==value:
+            if sodoku[row_offset + i][col_offset + j] == value:
                 return False
 
-
     return True
+
 
 # finds the next blank along, left to right then up to down
 def nextBlank(sodoku):
@@ -67,6 +73,7 @@ def nextBlank(sodoku):
                 return (i, j)
     return None
 
+
 # adds entry to a sodoku - returns boolean of success
 def addEntry(sodoku, row, col, value):
     if isValid(sodoku, row, col, value):
@@ -75,29 +82,32 @@ def addEntry(sodoku, row, col, value):
     else:
         return False
 
+
 #make dummy copy and save into answer list
 def runRecursiveHistory(sodoku, ans):
-    dummy = np.copy(sodoku) 
-    recursiveHistory(dummy, ans) 
+    dummy = np.copy(sodoku)
+    recursiveHistory(dummy, ans)
     return dummy
 
+
 #recursively solve - record history
-def recursiveHistory(sodoku, list): 
+def recursiveHistory(sodoku, list):
 
     pos = nextBlank(sodoku)
     if pos == None:
         return True
-    
-    for i in range(1,10):
-        list.append(str(i)  + str(pos[0])  + str(pos[1]) + "\n")
-        if isValid(sodoku, pos[0], pos[1], i):  
-            sodoku[pos[0]][pos[1]] = i  
-            list.append(str(i) + str(pos[0]) +  str(pos[1]) + "\n")                
+
+    for i in range(1, 10):
+        list.append(str(i) + str(pos[0]) + str(pos[1]) + "\n")
+        if isValid(sodoku, pos[0], pos[1], i):
+            sodoku[pos[0]][pos[1]] = i
+            list.append(str(i) + str(pos[0]) + str(pos[1]) + "\n")
             if recursiveHistory(sodoku, list):
                 return True
         sodoku[pos[0]][pos[1]] = 0
-        list.append("0" + str(pos[0]) + str(pos[1]) + "\n")  
-    return False  
+        list.append("0" + str(pos[0]) + str(pos[1]) + "\n")
+    return False
+
 
 # recursively solve - no recording
 def recursive(sodoku):
@@ -105,17 +115,18 @@ def recursive(sodoku):
     if pos == None:
         return True
 
-    for i in range(1,10):
-        if isValid(sodoku, pos[0], pos[1], i):  
-            sodoku[pos[0]][pos[1]] = i                  
+    for i in range(1, 10):
+        if isValid(sodoku, pos[0], pos[1], i):
+            sodoku[pos[0]][pos[1]] = i
             if recursive(sodoku):
                 return True
         sodoku[pos[0]][pos[1]] = 0
     return False
 
+
 # print sodoku to terminal
 def print_sodoku(sodoku):
     for row in range(9):
         for col in range(9):
-            print(sodoku[row][col], end = " ")
+            print(sodoku[row][col], end=" ")
         print()
